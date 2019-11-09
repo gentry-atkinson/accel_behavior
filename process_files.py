b8a481ea0794c5d0be48d12df9de0ce202e751ae
Gentry Atkinson <gma23@txstate.edu>
2019-11-09 12:56:56 -0600

    finished magnitude extractor for iphone data set

--- process_files.py
+++ process_files.py
@@ -1,3 +1,11 @@
+import numpy as np
+import math
+
+DIRECTORIES = ["dws_1", "dws_2", "dws_11", "jog_9", "jog_16", "sit_5", "sit_13", "std_6", "std_14", "ups_3", "ups_4", "ups_12", "wlk_7", "wlk_8", "wlk_15"]
+
+def resultant_vector(x_axis, y_axis, z_axis):
+    return math.sqrt((x_axis * x_axis) + (y_axis * y_axis) + (z_axis * z_axis))
+
 #splitting data 80/20 eventually
 all_data = np.zeros((360, 370, 12), dtype='float')
 all_labels = np.zeros((360), dtype='int')
@@ -33,3 +41,33 @@ for directory in DIRECTORIES:
         else:
             print("Bad directory name in read_files()")
     directory_num += 24
+
+train_data = np.zeros((288, 370), dtype='float')
+train_labels = np.zeros((288), dtype='int')
+test_data = np.zeros((72, 370), dtype='float')
+test_labels = np.zeros((72), dtype='int')
+
+test_counter = 0;
+train_counter = 0;
+
+for i in range(360):
+    for j in range(370):
+        if i%5 == 4:
+            test_data[test_counter][j] = resultant_vector(all_data[i][j][9], all_data[i][j][10], all_data[i][j][11])
+            test_labels[test_counter] = all_labels[i]
+        else:
+            train_data[test_counter][j] = resultant_vector(all_data[i][j][9], all_data[i][j][10], all_data[i][j][11])
+            train_labels[train_counter] = all_labels[i]
+    if i%5 == 4:
+        test_counter += 1
+    else:
+        train_counter += 1
+
+print(test_counter, " test samples recorded")
+print(train_counter, " train counters recorded")
+print(len(train_data[0]), " samples per segment")
+
+np.savetxt("raw_train_data_1d.csv", train_data, delimiter=',')
+np.savetxt("raw_train_labels_1d.csv", train_labels, delimiter=',')
+np.savetxt("raw_test_data_1d.csv", test_data, delimiter=',')
+np.savetxt("raw_test_labels_1d.csv", test_labels, delimiter=',')
