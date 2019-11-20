007858870b5f3af324a25959e0d6a7f6a1498c2e
Gentry Atkinson <gma23@txstate.edu>
2019-11-20 16:45:19 -0600

    little tweak on svm

--- train_svm_on_short_features.m
+++ train_svm_on_short_features.m
@@ -1,4 +1,4 @@
-raw_model = fitcecoc(rawtraindata1d, rawtrainlabels1d)
+raw_model = fitcecoc(decodeddata, rawtrainlabels1d)
 feature_model = fitcecoc(trainingdata, rawtrainlabels1d)
 
 raw_cv = crossval(raw_model)
@@ -7,5 +7,5 @@ feat_cv = crossval(feature_model)
 raw_error = kfoldLoss(raw_cv)
 feat_error = kfoldLoss(feat_cv)
 
-fprintf("Train on raw data: %d percent loss\n", raw_error*10)
-fprintf("Train on extracted features: %d percent loss\n", feat_error*10)
+fprintf("Train on raw data: %d percent loss\n", raw_error*100)
+fprintf("Train on extracted features: %d percent loss\n", feat_error*100)
