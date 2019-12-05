dc73d64de8de4e2d4a94005e743ea52f72c87a62
Gentry Atkinson <gma23@txstate.edu>
2019-12-04 20:44:16 -0600

    small change to svm accuracy

--- train_svm_on_short_features.m
+++ train_svm_on_short_features.m
@@ -7,5 +7,5 @@ feat_cv = crossval(feature_model)
 raw_error = kfoldLoss(raw_cv)
 feat_error = kfoldLoss(feat_cv)
 
-fprintf("Train on raw data: %d percent loss\n", raw_error*100)
-fprintf("Train on extracted features: %d percent loss\n", feat_error*100)
+fprintf("Train on raw data: %d percent accuracy\n", 1-raw_error)
+fprintf("Train on extracted features: %d percent accuracy\n", 1-feat_error*100)
